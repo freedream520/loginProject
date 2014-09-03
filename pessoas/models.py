2358d760cc5b363d52f2c692c7f8e504fe3084b2
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-03 19:56:10 -0300

    Utilizando comando syndb e modificação de arquivos

--- pessoas/models.py
+++ pessoas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Pessoa(AbstractUser):
+		endereco = models.CharField(max_length=100, blank=True, null=True)
 
 # Create your models here.
