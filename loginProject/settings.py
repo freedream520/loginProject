cde058117cc9f757e93c2968d0215eaef3887024
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-03 19:49:54 -0300

    Alteração do arquivo settings.py

--- loginProject/settings.py
+++ loginProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+AUTH_USER_MODEL = 'pessoas.Pessoa'
+LOGIN_URL = '/login/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
