99eac963b0127aa19d976c599eba8f1795f9cd55
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-03 20:09:43 -0300

    Criação das urls do projeto

--- loginProject/urls.py
+++ loginProject/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+urlpatterns = patterns('pessas.views',
+    url(r'^$', 'index'),
+    url(r'^login/$', 'login'),
+    url(r'^validar/$', 'validar'),
+    url(r'^logoff/$', 'logoff'),
+    url(r'^dashboard/$', 'dashboard'),
+    url(r'^cadastro/$', 'cadastro'),
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'loginProject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
