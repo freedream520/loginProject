7f3224ad4b9a52d5d726a861cda32745b169f49d
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-03 21:34:30 -0300

    Alteração dos arquivos views e urls

--- pessoas/views.py
+++ pessoas/views.py
@@ -10,4 +10,24 @@ def login(request):
 		form = LoginForm()
 		return render(request, 'login.html', {'form':form})
 
+def validar(request):
+		if request.method == 'POST':
+				form = LoginForm(request.POST)
+
+				if form.is_valid():
+						pessoa = authenticate(username=form.data['login'], password=form.data['senha'])
+
+						if pessoa is not None:
+								if pessoa.is_active:
+										meu_login(request, pessoa)
+										return HttpResponseRedirect('/dashboard/')
+								else:
+										return render(request, 'login.html', {'form':form})
+						else:
+								return render(request, 'login.html', {'form':form})
+				else:
+						return render(request, 'login.html', {'form':form})
+		else:
+			return HttpResponseRedirect('/login/')
+
 # Create your views here.
