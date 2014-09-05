9d35eab22faf5bb23396edcb1ddfade9d4af12e8
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-05 00:01:37 -0300

    Alterando arquivo

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from pessoas.forms import PessoaForm, LoginForm
 from django.contrib.auth import authenticate, logout, login as meu_login
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 		return render(request, 'index.html')
