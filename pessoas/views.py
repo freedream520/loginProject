838ec3fa7d34211e8d409e18b11810d3ae75966a
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-10 20:27:09 -0300

    Alterando a views.py para registrar o e-mail

--- pessoas/views.py
+++ pessoas/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, logout, login as meu_login
 from django.contrib.auth.decorators import login_required
 from pessoas.models import Pessoa
 from django.core.mail import send_mail
-
+	
 def index(request):
 		return render(request, 'index.html')
 
@@ -39,4 +39,33 @@ def logoff(request):
 @login_required()
 def dashboard(request):
 		return render(request, 'dashboard.html')
-# Create your views here.
+# Create your views hereself.
+
+def cadastro(request):
+    form = cadastroForm()
+    return render(request,'cadastro.html',{'form':form})
+
+def cadastro_validar(request):
+    if request.method == 'POST':
+        form = cadastroForm(request.POST)
+
+        if form.is_valid():
+            pessoa = Pessoa(
+                username=form.data['login'], 
+                email=form.data['email'],
+                is_active=False
+            )
+            pessoa.set_password(form.data['senha'])
+            pessoa.save()
+            
+            if send_mail('Campo assunto Aqui', 'Valide o seu email: http://unifran-78230.sae1.nitrousbox.com/token/'+str(pessoa.pk), 'testeunifran@bol.com.br',
+    [pessoa.email], fail_silently=False):
+                # Se der erro ele avisa aqui.
+                # Retornar tela de sucesso! Vc Consegue fazer e tb arrumar esses códigos?
+                return render(request,'cadastro.html',{'form':form})
+
+def token(request, numero):
+    pessoa = Pessoa.objects.get(pk=numero)
+    pessoa.is_active = True
+    pessoa.save()
+    return HttpResponseRedirect('/login/')
