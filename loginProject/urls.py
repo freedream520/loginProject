a20a7dd1ad841b290ce06eaa49f94a86d25da474
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-10 20:54:30 -0300

    Alterando as urls.py

--- loginProject/urls.py
+++ loginProject/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('pessoas.views',
-    url(r'^$', 'index'),
-    url(r'^login/$', 'login'),
-    url(r'^validar/$', 'validar'),
-    url(r'^logoff/$', 'logoff'),
-    url(r'^dashboard/$', 'dashboard'),
-    url(r'^cadastro/$', 'cadastro'),
-
+	    url(r'^$', 'index'),
+	    url(r'^login/$', 'login'),
+	    url(r'^validar/$', 'validar'),
+	    url(r'^logoff/$', 'logoff'),
+	    url(r'^dashboard/$', 'dashboard'),
+	    url(r'^cadastro/$', 'cadastro'),
+	    url(r'^cadastro/$', 'cadastro'), 
+	    url(r'^cadastro_validar/$', 'cadastro_validar'),
+	    url(r'^token/(?P<numero>\d+)/$', 'token'),
 )
