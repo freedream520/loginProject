be1bc9d6f00b305447f7e5642ad5a27520e8c86f
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-09-17 19:43:46 -0300

    Alterando o arquivo urls.py

--- loginProject/urls.py
+++ loginProject/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('pessoas.views',
 	    url(r'^cadastro/$', 'cadastro'),
 	    url(r'^cadastro_validar/$', 'cadastro_validar'),
 	    url(r'^token/(?P<numero>\d+)/$', 'token'),
+	    url(r'^i18n/', include('django.config.urls.il18n')),
 )
